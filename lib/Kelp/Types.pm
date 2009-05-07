2b39e5c67034b6ae20c52953be9bc3a2989f5279
Chris Prather <cprather@hdpublishing.com>
2009-05-07 13:08:13 -0400

    add tests for conversions and growth

--- lib/Kelp/Types.pm
+++ lib/Kelp/Types.pm
@@ -3,17 +3,17 @@ use Data::Dimensions qw(&units);
 
 use MooseX::Types -declare => [
     qw(
-      Bytes
-      KibiBytes
-      MebiBytes
-      GibiBytes
-      TebiBytes
-      PebiBytes
-      ExbiBytes
-      )
+        Bytes
+        KibiBytes
+        MebiBytes
+        GibiBytes
+        TebiBytes
+        PebiBytes
+        ExbiBytes
+        )
 ];
 
-use MooseX::Types::Moose qw(Int);
+use MooseX::Types::Moose qw(Int Any);
 
 class_type 'Data::Dimensions';
 
@@ -24,19 +24,18 @@ use constant tebi => 2**40;
 use constant pebi => 2**50;
 use constant exbi => 2**60;
 
-subtype Bytes,     as 'Data::Dimensions', where { $_->{units}->{byte} == 1 };
-subtype KibiBytes, as 'Data::Dimensions', where { $_->{units}->{byte} == kibi };
-subtype MebiBytes, as 'Data::Dimensions', where { $_->{units}->{byte} == mebi };
-subtype GibiBytes, as 'Data::Dimensions', where { $_->{units}->{byte} == gibi };
-subtype TebiBytes, as 'Data::Dimensions', where { $_->{units}->{byte} == tebi };
-subtype PebiBytes, as 'Data::Dimensions', where { $_->{units}->{byte} == pebi };
-subtype ExbiBytes, as 'Data::Dimensions', where { $_->{units}->{byte} == exbi };
+subtype Bytes,     as 'Data::Dimensions', where { $_->{units}{byte} == 1 };
+subtype KibiBytes, as 'Data::Dimensions', where { $_->{units}{byte} == kibi };
+subtype MebiBytes, as 'Data::Dimensions', where { $_->{units}{byte} == mebi };
+subtype GibiBytes, as 'Data::Dimensions', where { $_->{units}{byte} == gibi };
+subtype TebiBytes, as 'Data::Dimensions', where { $_->{units}{byte} == tebi };
+subtype PebiBytes, as 'Data::Dimensions', where { $_->{units}{byte} == pebi };
+subtype ExbiBytes, as 'Data::Dimensions', where { $_->{units}{byte} == exbi };
 
 coerce Bytes,     from Int, via { units( { byte => 1 },    $_ ); };
 coerce KibiBytes, from Int, via { units( { byte => kibi }, $_ ); };
 coerce MebiBytes, from Int, via { units( { byte => mebi }, $_ ); };
 coerce GibiBytes, from Int, via { units( { byte => gibi }, $_ ); };
-coerce TebiBytes, from Int, via { units( { byte => tebi }, $_ ); };
 coerce PebiBytes, from Int, via { units( { byte => pebi }, $_ ); };
 coerce ExbiBytes, from Int, via { units( { byte => exbi }, $_ ); };
 
@@ -46,3 +45,17 @@ coerce Bytes, from GibiBytes, via { units( { byte => 1 }, $_ * gibi ) };
 coerce Bytes, from TebiBytes, via { units( { byte => 1 }, $_ * tebi ) };
 coerce Bytes, from PebiBytes, via { units( { byte => 1 }, $_ * pebi ) };
 coerce Bytes, from ExbiBytes, via { units( { byte => 1 }, $_ * exbi ) };
+
+coerce KibiBytes, from Bytes, via { units( { byte => kibi }, $_ / kibi ) };
+coerce MebiBytes, from Bytes, via { units( { byte => mebi }, $_ / mebi ) };
+coerce GibiBytes, from Bytes, via { units( { byte => gibi }, $_ / gibi ) };
+coerce TebiBytes, from Bytes, via { units( { byte => tebi }, $_ / tebi ) };
+coerce PebiBytes, from Bytes, via { units( { byte => pebi }, $_ / pebi ) };
+coerce ExbiBytes, from Bytes, via { units( { byte => exbi }, $_ / exbi ) };
+
+coerce KibiBytes, from Any, via { to_KibiBytes( to_Bytes($_) ) };
+coerce MebiBytes, from Any, via { to_MebiBytes( to_Bytes($_) ) };
+coerce GibiBytes, from Any, via { to_GibiBytes( to_Bytes($_) ) };
+coerce TebiBytes, from Any, via { to_TebiBytes( to_Bytes($_) ) };
+coerce PebiBytes, from Any, via { to_PebiBytes( to_Bytes($_) ) };
+coerce ExbiBytes, from Any, via { to_ExbiBytes( to_Bytes($_) ) };
--- t/01-information-science.t
+++ t/01-information-science.t
@@ -1,6 +1,6 @@
 #!/usr/bin/perl -w
 use strict;
-use Test::More tests => 8;
+use Test::More tests => 11;
 
 use Kelp::Types ':all';
 
@@ -14,5 +14,14 @@ use Kelp::Types ':all';
     ok( is_KibiBytes($kb), 'it is a kilobyte' );
     is( $kb, 3, 'got a kilobyte' );
     ok( my $bytes = to_Bytes($kb) );
-    is( $bytes, 3 * 1024, 'right size' );
+    is( $bytes, 1024 * 3, 'right size' );
+}
+
+{
+    ok( my $uuid_size = to_Bytes(16), '16 byte uuid' );
+    ok(
+        my $storage_required = to_GibiBytes( $uuid_size * 10**9 ),
+        'billion uuids is how big?'
+    );
+    is($storage_required, 14.9011611938477, 'right size')
 }
