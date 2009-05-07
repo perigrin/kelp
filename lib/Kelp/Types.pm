8e39883c42f493f4b5acfcc443f8745f8b814cd3
Chris Prather <chris@prather.org>
2009-05-06 22:59:19 -0400

    change to the ISO standard kibi

--- lib/Kelp/Types.pm
+++ lib/Kelp/Types.pm
@@ -4,7 +4,7 @@ use Data::Dimensions qw(&units);
 use MooseX::Types -declare => [
     qw(
       Bytes
-      KiloBytes
+      KibiBytes
       )
 ];
 
@@ -12,10 +12,9 @@ use MooseX::Types::Moose qw(Int);
 
 class_type 'Data::Dimensions';
 
-subtype Bytes, as 'Data::Dimensions', where { $_->{units}->{byte} == 1 };
-subtype KiloBytes, as 'Data::Dimensions',
-  where { $_->{units}->{byte} == 1024 };
+subtype Bytes,     as 'Data::Dimensions', where { $_->{units}->{byte} == 1 };
+subtype KibiBytes, as 'Data::Dimensions', where { $_->{units}->{byte} == 2^10 };
 
 coerce Bytes,     from Int,       via { units( { byte => 1 },    $_ ); };
-coerce Bytes,     from KiloBytes, via { units( { byte => 1 },    $_ * 1024 ) };
-coerce KiloBytes, from Int,       via { units( { byte => 1024 }, $_ ); };
+coerce Bytes,     from KibiBytes, via { units( { byte => 1 },    $_ * 1024 ) };
+coerce KibiBytes, from Int,       via { units( { byte => 1024 }, $_ ); };
--- t/01-information-science.t
+++ t/01-information-science.t
@@ -10,8 +10,8 @@ use Kelp::Types ':all';
     is( $byte, 1, 'right size' );
 }
 {
-    ok( my $kb = to_KiloBytes(3), 'create 3 Kilobyte' );
-    ok( is_KiloBytes($kb), 'it is a kilobyte' );
+    ok( my $kb = to_KibiBytes(3), 'create 3 Kilobyte' );
+    ok( is_KibiBytes($kb), 'it is a kilobyte' );
     is( $kb, 3, 'got a kilobyte' );
     ok( my $bytes = to_Bytes($kb) );
     is( $bytes, 3 * 1024, 'right size' );
