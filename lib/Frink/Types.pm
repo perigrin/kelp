fb947e8ef967d1b305a62d29208ed4f1569ef587
Chris Prather <chris@prather.org>
2009-05-06 22:52:14 -0400

    start laying out frink types

---
+++ lib/Frink/Types.pm
@@ -0,0 +1,21 @@
+package Frink::Types;
+use Data::Dimensions qw(&units);
+
+use MooseX::Types -declare => [
+    qw(
+      Bytes
+      KiloBytes
+      )
+];
+
+use MooseX::Types::Moose qw(Int);
+
+class_type 'Data::Dimensions';
+
+subtype Bytes, as 'Data::Dimensions', where { $_->{units}->{byte} == 1 };
+subtype KiloBytes, as 'Data::Dimensions',
+  where { $_->{units}->{byte} == 1024 };
+
+coerce Bytes,     from Int,       via { units( { byte => 1 },    $_ ); };
+coerce Bytes,     from KiloBytes, via { units( { byte => 1 },    $_ * 1024 ) };
+coerce KiloBytes, from Int,       via { units( { byte => 1024 }, $_ ); };
---
+++ t/01-information.t
@@ -0,0 +1,18 @@
+#!/usr/bin/perl -w
+use strict;
+use Test::More tests => 8;
+
+use Frink::Types ':all';
+
+{
+    ok( my $byte = to_Bytes(1), 'create a byte' );
+    ok( is_Bytes($byte), 'is a byte' );
+    is( $byte, 1, 'right size' );
+}
+{
+    ok( my $kb = to_KiloBytes(3), 'create 3 Kilobyte' );
+    ok( is_KiloBytes($kb), 'it is a kilobyte' );
+    is( $kb, 3, 'got a kilobyte' );
+    ok( my $bytes = to_Bytes($kb) );
+    is( $bytes, 3 * 1024, 'right size' );
+}
